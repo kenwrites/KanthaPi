315c1093e13dfca2c66ab124d9b6de182b828f57
kenwrites <28812088+kenwrites@users.noreply.github.com>
2019-05-20 09:58:18 -0400

    User input collection loop done

--- KanthaPi/Program.cs
+++ KanthaPi/Program.cs
@@ -4,9 +4,46 @@ namespace KanthaPi
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            bool KeepGoing = true;
+
+            do
+            {
+                // Get user input
+
+                Console.WriteLine("How many significant digits of Pi should I calculate?  Type 'exit' to quit.");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                // Validate
+
+                bool InputIsValid = int.TryParse(input, out int digits);
+                if (digits < 0)
+                {
+                    Console.WriteLine("Please enter a whole number greater than or equal to 0.");
+                    break;
+                }
+
+                // Calculate Pi
+
+                if (InputIsValid)
+                {
+                    Console.WriteLine("Valid input");
+
+                }
+                else
+                {
+                    // Let user know about input issues
+                    Console.WriteLine("Sorry, there was a problem with your entry.  Please enter a whole number.");
+                }
+
+            }
+            while (KeepGoing);
+
+        } // end Main()
+    } // end Program
+} // end namespace
